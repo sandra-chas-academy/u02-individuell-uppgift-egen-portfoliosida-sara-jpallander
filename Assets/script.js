5e154688c00332db442fdb21be59c8711942b7d2
sara-jpallander <sara.j.pallander@gmail.com>
2024-11-11 17:08:52 +0100

    add about me content and filled out json file

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,36 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    let menuBtn = document.getElementById('menu-btn');
+    const navList = document.getElementById('nav-list');
+    const emailModule = document.getElementById('email-module');
+    const emailLogo = document.getElementById('email-logo');
 
+    let menuHidden = true;
+
+    menuBtn.addEventListener('click', function() {
+
+        if(menuHidden) {
+            
+            navList.style.display = "block";
+            menuBtn.innerHTML = "▲";
+            menuHidden = false;
+
+        } else {
+            navList.style.display = "none";
+            menuBtn.innerHTML = "▼";
+            menuHidden = true;
+        }
+        
+  
+        console.log("click!");
+    });
     
+    emailLogo.addEventListener('mouseenter', function () {
+        emailModule.style.display = "block";
+    });
+
+    emailLogo.addEventListener('mouseleave', function () {
+        emailModule.style.display = "none";
+    });
+
 });
